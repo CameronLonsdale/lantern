ffd50b9ccf2e9ea91a141148fd7c4d3e351d9a32
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-02-19 20:51:36 +1100

    Utilities to remove characters from text and tests.

--- cckrypto/util.py
+++ cckrypto/util.py
@@ -1,10 +1,23 @@
 """Utility functions."""
+
 import string
 
-exclude = set(string.punctuation + string.whitespace)
+
+def remove(text, exclude):
+    """Remove letters from exclude in text."""
+    return ''.join(ch for ch in text if ch not in exclude).rstrip()
+
+
+def remove_punct_and_whitespace(text):
+    """Remove punctuation and whitespace from a string."""
+    return remove(text, string.punctuation + string.whitespace)
 
 
 def remove_punctuation(text):
     """Remove punctuation and whitespace."""
-    text = ''.join(ch for ch in text if ch not in exclude)
-    return text.rstrip()
+    return remove(text, string.punctuation)
+
+
+def remove_whitespace(text):
+    """Remove whitespace from text."""
+    return remove(text, string.whitespace)
---
+++ tests/test_util.py
@@ -0,0 +1,30 @@
+"""Test utility functions."""
+
+from cckrypto.util import (
+    remove_punctuation, remove_whitespace,
+    remove_punct_and_whitespace
+)
+
+
+def test_remove_punctuation():
+    """Test punctuation removed."""
+    plaintext = "Don't worry my friends."
+    assert remove_punctuation(
+        plaintext
+    ) == "Dont worry my friends"
+
+
+def test_remove_whitespace():
+    """Test whitespace removed."""
+    plaintext = "Don't worry my friends."
+    assert remove_whitespace(
+        plaintext
+    ) == "Don'tworrymyfriends."
+
+
+def test_remove_punct_and_whitespace():
+    """Test punctuation and whitespace removal."""
+    plaintext = "Don't worry my friends."
+    assert remove_punct_and_whitespace(
+        plaintext
+    ) == "Dontworrymyfriends"
