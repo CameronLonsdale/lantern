96d8b59c89929a289a0f94fea1373fe9d060449d
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-04-25 09:21:24 +1000

    test for chisquared scorer. Some design issues arose. Need to write tests for analysis module to better fix up these issues

--- lantern/analysis/frequency.py
+++ lantern/analysis/frequency.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 
-
-def frequency_analyze(text, n=1):
+# TODO: Am I handling stripping away punctuation here? Should that be default or optional?
+# I think punction should be kept by default. If a user wants to take the frequency with all punctuation characters remove they can either use the keep_punct flag or strip it themselves
+def frequency_analyze(text, n=1, keep_punct=True):
     # TODO: work with different sized ngrams
     frequency = defaultdict(lambda: 0, {})
     for symbol in text:
@@ -27,11 +28,12 @@ def avg_index_of_coincidence(texts):
         average += index_of_coincidence(text)
     return average / len(texts)
 
-
+## TODO: SOLVE THE KEY ERROR PROBLEM
+## IF a key is not present in the target frequency then its expected value is 0
 def chi_squared(source_frequency, target_frequency):
+    target_prob = frequency_to_probability(target_frequency)
     source_len = sum(source_frequency.values())
-    body = lambda n: (source_frequency[n] - source_len * target_frequency[n])**2 / (source_len * target_frequency[n])
-    return sum(body(n) for n in source_frequency)
+    return sum(_calculate_chi_squared(source_frequency, target_prob, source_len, n) for n in source_frequency)
 
 
 ENGLISH_IC = 0.066
@@ -40,3 +42,7 @@ ENGLISH_IC = 0.066
 def _calculate_ioc(frequency_map, N):
     coms_of_letters = sum((frequency_map[n] * (frequency_map[n] - 1)) for n in frequency_map)
     return float(coms_of_letters) / (N * (N - 1))
+
+
+def _calculate_chi_squared(source_frequency, target_prob, source_len, n):
+    return (source_frequency[n] - source_len * target_prob[n])**2 / (source_len * target_prob[n])
---
+++ tests/score_functions/test_chisquared.py
@@ -0,0 +1,16 @@
+"""Test the chi-squared scoring function"""
+
+from lantern.analysis import (
+    english_frequency, frequency_to_probability,
+    chi_squared, frequency_analyze
+)
+from lantern.score_functions import ChiSquared
+
+
+def test_chisquared_english_unigrams():
+    unigram_freq = english_frequency.unigrams()
+    scorer = ChiSquared(unigram_freq)
+
+    # TODO: This should be replaced by using the keep_punct flag once its added
+    text = "This is an example".upper().replace(' ', '')
+    assert scorer(text) == -chi_squared(frequency_analyze(text), unigram_freq)
