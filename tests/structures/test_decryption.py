72f8d8e6ed9fce773f1705d66b78eff13c473adf
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-04-24 20:18:30 +1000

    Tests for the decryption structure

--- tests/structures/test_decryption.py
+++ tests/structures/test_decryption.py
@@ -3,8 +3,15 @@
 from lantern.structures import Decryption
 
 
-def test_using_decryption():
+def test_kwargs():
     decryption = Decryption(plaintext="plaintext", score=0, key="key")
     assert decryption.plaintext == "plaintext"
     assert decryption.score == 0
     assert decryption.key == "key"
+
+
+def test_args():
+    decryption = Decryption("plaintext", "key", 0)
+    assert decryption.plaintext == "plaintext"
+    assert decryption.key == "key"
+    assert decryption.score == 0
