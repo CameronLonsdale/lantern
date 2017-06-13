c28101b7a449c3f8e9552153b667c532ea93b3b6
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-06-13 16:55:31 +1000

    Refactored caesar to play nice with text that is mixed case. This will make it easier to support different languages in the future

--- lantern/modules/caesar.py
+++ lantern/modules/caesar.py
@@ -1,4 +1,4 @@
-"""Automated breaking of the Caesar Cipher"""
+"""Automated breaking of the Caesar Cipher."""
 
 import string
 
@@ -8,7 +8,7 @@ from lantern.structures import Decryption
 
 def crack(ciphertext, score_functions, min_key=0, max_key=26):
     """
-    Break ``ciphertext`` by enumeration keys between ``min_key`` and ``max_key``.
+    Break ``ciphertext`` by enumerating keys between ``min_key`` and ``max_key``.
 
     Example: ::
 
@@ -18,7 +18,7 @@ def crack(ciphertext, score_functions, min_key=0, max_key=26):
         ciphertext (str): The text to decrypt
         scoring_functions (Function or iterable of functions): Function(s) to score decryptions with
         min_key (int): Key to start with
-        max_key (int): Key to stop at
+        max_key (int): Key to stop at (exclusive)
 
     Return:
         Sorted list of decryptions
@@ -30,7 +30,6 @@ def crack(ciphertext, score_functions, min_key=0, max_key=26):
         raise ValueError("min_key cannot exceed max_key")
 
     decryptions = []
-
     for key in range(min_key, max_key):
         plaintext = decrypt(key, ciphertext)
         decryptions.append(Decryption(plaintext, key, score(plaintext, score_functions)))
@@ -38,8 +37,6 @@ def crack(ciphertext, score_functions, min_key=0, max_key=26):
     return sorted(decryptions, reverse=True)
 
 
-# TODO: make decrypt a closure which decrypts based on a certain source language
-# because right now it is hard coded to english
 def decrypt(key, ciphertext):
     """
     Decrypt Caesar encrypted ``ciphertext`` using ``key``.
@@ -55,10 +52,11 @@ def decrypt(key, ciphertext):
     Return:
         plaintext
     """
-    alphabet = string.ascii_letters
-    key %= len(string.ascii_lowercase)
-    shifted_lower = string.ascii_lowercase[key:] + string.ascii_lowercase[:key]
-    shifted_upper = string.ascii_uppercase[key:] + string.ascii_uppercase[:key]
-    shifted = shifted_lower + shifted_upper
+    alphabet = [string.ascii_lowercase, string.ascii_uppercase]
+    key %= len(alphabet[0])
+    shifted = _shift_alphabet(alphabet, key)
+    return ciphertext.translate(str.maketrans(''.join(shifted), ''.join(alphabet)))
 
-    return ciphertext.translate(str.maketrans(shifted, alphabet))
+
+def _shift_alphabet(letter_cases, shift):
+    return [case[shift:] + case[:shift] for case in letter_cases]
