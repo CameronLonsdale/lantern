4f107467f033d3e9e956cf52d7b8aed50d5901c2
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-06-27 08:39:48 +1000

    Documentation changes to corpus.py. Not particuarly interested on improving the function yet as I know more changes are needed in the future first, regarding weighted mean

--- lantern/fitness/corpus.py
+++ lantern/fitness/corpus.py
@@ -1,7 +1,8 @@
 """Score plaintext based on number of words identified are in the corpus"""
-from math import log10
 
 import string
+
+from math import log10
 
 from lantern.util import remove
 
@@ -10,12 +11,31 @@ class Corpus():
     """Scoring function based on existance of words in a corpus."""
 
     def __init__(self, corpus):
-        """Build function with set of words from a corpus."""
+        """Build function with set of words from a corpus.
+
+        Parameters:
+            corpus (collection): collection of words to use
+        """
         self.words = corpus
         self.floor = log10(0.01 / len(self.words))
 
     def __call__(self, text):
-        """Score based on number of words not in the corpus."""
+        """Score based on number of words not in the corpus.
+
+        Example:
+            >>> fitness = Corpus(["example"])
+            >>> fitness("example")
+            0
+
+            >>> fitness("different")
+            -2.0
+
+        Parameters:
+            text (str): The text to score
+
+        Return:
+            Corpus score for text
+        """
         text = remove(text, string.punctuation)
         words = text.split()
 
